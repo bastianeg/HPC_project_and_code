43f5d589d3abb1ce06de42174a202d06e916e8d0
Bastian G <bastianeg1337@gmail.com>
2020-01-08 15:45:20 +0100

    Added native and the loop permutations

---
+++ combined/native_combined.c
@@ -0,0 +1,92 @@
+
+void matmult_nat(int m, int n, int k, double **A, double **B, double **C){
+for (int i=0;i<m;i++){
+    for (int p=0;p<n;p++){
+        C[i][p]=0;
+    }
+}
+for (int i=0;i<m;i++) {
+    for (int p=0;p<n;p++) { 
+        for (int j=0;j<k;j++){
+            C[i][p]+=A[i][j]*B[j][p];
+        }
+    }
+}
+}
+
+void matmult_mkn(int m, int n, int k, double **A, double **B, double **C){
+for (int i=0;i<m;i++){
+    for (int p=0;p<n;p++){
+        C[i][p]=0;
+    }
+}
+for (int i=0;i<m;i++) {
+    for (int j=0;j<k;j++){
+        for (int p=0;p<n;p++) { 
+            C[i][p]+=A[i][j]*B[j][p];
+        }
+    }
+}
+}
+
+void matmult_nkm(int m, int n, int k, double **A, double **B, double **C){
+for (int i=0;i<m;i++){
+    for (int p=0;p<n;p++){
+        C[i][p]=0;
+    }
+}
+for (int p=0;p<n;p++) {
+    for (int j=0;j<k;j++){
+        for (int i=0;i<m;i++) { 
+            C[i][p]+=A[i][j]*B[j][p];
+        }
+    }
+}
+}
+
+void matmult_nmk(int m, int n, int k, double **A, double **B, double **C){
+for (int i=0;i<m;i++){
+    for (int p=0;p<n;p++){
+        C[i][p]=0;
+    }
+}
+for (int p=0;p<n;p++) {
+    for (int i=0;i<m;i++){
+        for (int j=0;j<k;j++){
+            C[i][p]+=A[i][j]*B[j][p];
+        }
+    }
+}
+}
+
+void matmult_kmn(int m, int n, int k, double **A, double **B, double **C){
+for (int i=0;i<m;i++){
+    for (int p=0;p<n;p++){
+        C[i][p]=0;
+    }
+}
+for (int j=0;j<k;j++) {
+    for (int i=0;i<m;i++){
+        for (int p=0;p<n;p++){
+            C[i][p]+=A[i][j]*B[j][p];
+        }
+    }
+}
+}
+
+void matmult_knm(int m, int n, int k, double **A, double **B, double **C){
+for (int i=0;i<m;i++){
+    for (int p=0;p<n;p++){
+        C[i][p]=0;
+    }
+}
+
+for (int j=0;j<k;j++) {
+    for (int p=0;p<n;p++){
+        for (int i=0;i<m;i++){
+            C[i][p]+=A[i][j]*B[j][p];
+        }
+    }
+}
+}
+
