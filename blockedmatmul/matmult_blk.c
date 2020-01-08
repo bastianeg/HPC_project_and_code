1673b861ac56872d3c5536314758bd9f036d6ece
Bastian G <bastianeg1337@gmail.com>
2020-01-08 13:48:00 +0100

    fixed matmult_blk such that it works

--- blockedmatmul/matmult_blk.c
+++ blockedmatmul/matmult_blk.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
 void matmult_blk(int m,int n,int k,double **A,double **B,double **C, int bs){
@@ -6,10 +7,15 @@ void matmult_blk(int m,int n,int k,double **A,double **B,double **C, int bs){
     int bi,bj,bl;
 
     // The number of blocks is ceil of N/bs
-    int mb = m/bs + (int)m%bs!=0;
-    int nb = n/bs + (int)n%bs!=0;
-    int kb = k/bs + (int)k%bs!=0;
-    
+    int mb = m/bs + (int)(m%bs!=0);
+    int nb = n/bs + (int)(n%bs!=0);
+    int kb = k/bs + (int)(k%bs!=0);
+
+    //initialize C as zeros
+    for(i=0; i<m; i++)
+        for(j=0; j<n; j++)
+            C[i][j] = 0.0;
+
     //looping through the blocks
     for(bi=0; bi<mb; bi++){
         for(bj=0; bj<nb; bj++){
@@ -18,11 +24,10 @@ void matmult_blk(int m,int n,int k,double **A,double **B,double **C, int bs){
                 //looping within a block
                 //we make sure the i,j,l is the actual i,j,l
                 //to account non multiples of bs we use MIN
-                for(i=bi*bs; i<MIN((bi+1)*bs,n); i++){
-                    for(j=bj*bs; j<MIN((bj+1)*bs,m); j++){
-                        C[i][j] = 0.0;
+                for(i=bi*bs; i<MIN((bi+1)*bs,m); i++){
+                    for(j=bj*bs; j<MIN((bj+1)*bs,n); j++){
                         for(l=bl*bs; l<MIN((bl+1)*bs,k); l++){
-                            C[i][j] += A[i][l]*B[j][l];
+                            C[i][j] += A[i][l]*B[l][j];
                         }
                     }
                 }
