eb103903baa4a8151a31288249b4fae6c1907ec6
Bastian G <bastianeg1337@gmail.com>
2020-01-13 16:16:54 +0100

    hello world

--- omp_autopar/hello.c
+++ omp_autopar/hello.c
@@ -4,7 +4,7 @@
 #endif
 int main(int argc, char *argv[]) {
     int t_id = 0;
-    #pragma omp parallel
+    #pragma omp parallel private(t_id) num_threads(4)
     {
         t_id = omp_get_thread_num();
         printf("Hello world from %d!\n",t_id);
