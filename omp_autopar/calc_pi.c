e56a3987a0067e864657df21f5ba1f6f789aa9ff
Bastian G <bastianeg1337@gmail.com>
2020-01-13 13:05:28 +0100

    changes

--- omp_autopar/calc_pi.c
+++ omp_autopar/calc_pi.c
@@ -5,8 +5,10 @@ int main(int argc, char *argv[]){
     int N = 10000;
     double Nr = 1.0/N;
     double pi = 0.0;
+    double tmp;
     for(i=0;i<N;i++){
-        pi += 4/(1+Nr*Nr*(i-0.5)*(i-0.5));
+        tmp = 4/(1+Nr*Nr*(i-0.5)*(i-0.5));
+        pi += tmp;
     }
     pi *= Nr;
     printf("Pi is %.5f with %d iterations\n",pi,N);
