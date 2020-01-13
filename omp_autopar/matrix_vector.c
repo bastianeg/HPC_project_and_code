b468a8f12fc1a0f03848d6a6f34e7cd1e29fb180
Bastian G <bastianeg1337@gmail.com>
2020-01-13 16:41:37 +0100

    changes

--- omp_autopar/matrix_vector.c
+++ omp_autopar/matrix_vector.c
@@ -17,8 +17,8 @@ void mvx(int m, int n, double *a, double *b, double *c){
 
 int main(int argc, char* argv[]){
     int i,j;
-    int n = 10;
-    int m = 10;
+    int n = 1000;
+    int m = n;
     double *a = malloc(m*sizeof(double));
     double *c = malloc(n*sizeof(double));
     double *b = malloc(n*m*sizeof(double));
@@ -34,7 +34,7 @@ int main(int argc, char* argv[]){
     mvx(m,n,a,b,c);
     
     for(i=0;i<m;i++){
-        printf("%.1f",a[i]);
+        printf("%.1f\n",a[i]);
     }
 
     free(a);
