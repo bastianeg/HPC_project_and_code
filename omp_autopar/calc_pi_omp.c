2ac51b98e55aeae54304377968f617ea7ecc35c5
Bastian G <bastianeg1337@gmail.com>
2020-01-13 15:45:47 +0100

    more changes

--- omp_autopar/calc_pi_omp.c
+++ omp_autopar/calc_pi_omp.c
@@ -7,7 +7,7 @@ double pi_int(int N){
     int i;
     double Nr = 1.0/N;
     double pi = 0.0;
-    #pragma omp parallell for default(none) \
+    #pragma omp parallel for default(none) \
             shared(Nr,N) private(i,pi) \
             reduction(+: pi)
     for(i=1;i<=N;i++){
