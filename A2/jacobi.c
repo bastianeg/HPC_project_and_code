67441cf147f2332ebbefa58aa6a74c3c869bc751
Bastian G <bastianeg1337@gmail.com>
2020-01-15 15:55:31 +0100

    hello

--- A2/jacobi.c
+++ A2/jacobi.c
@@ -12,11 +12,11 @@ jacobi(double ***U, double ***F, double ***Uold, int N, int iter_max, double tol
     //define norm and max_iter and Uold and iter and threshold
     double U1, U2, U3, U4, U5, U6;
     int iter = 0;
-
+    double onesixth = 1.0/6.0;
     double d = tol+10; //inf
-    for (int i = 1; i<(N-1); i++){
-        for (int j = 1; j<(N-1); j++){
-            for (int k = 1; k<(N-1); k++){
+    for (int i = 0; i<(N+2); i++){
+        for (int j = 0; j<(N+2); j++){
+            for (int k = 0; k<(N+2); k++){
                 Uold[i][j][k] = U[i][j][k];
             }
         }
@@ -43,8 +43,8 @@ jacobi(double ***U, double ***F, double ***Uold, int N, int iter_max, double tol
                     U5 = Uold[i][j][k-1];
                     U6 = Uold[i][j][k+1];
 
-                    // U = 1/6 * (sum of us)
-                    U[i][j][k] = (1/6)*(U1+U2+U3+U4+U5+U6+deltasq*F[i][j][k]);
+                    // U = 1/6 * (sum of us +Delta^2 f)
+                    U[i][j][k] = onesixth*(U1+U2+U3+U4+U5+U6+deltasq*F[i][j][k]);
 
                     d += (U[i][j][k]-Uold[i][j][k])*(U[i][j][k]-Uold[i][j][k]);
                 }
@@ -57,12 +57,12 @@ jacobi(double ***U, double ***F, double ***Uold, int N, int iter_max, double tol
         // update iteration and Uold
         iter ++;
         
-        for (int i = 1; i<(N-1); i++){
-            for (int j = 1; j<(N-1); j++){
-                for (int k = 1; k<(N-1); k++){
+        for (int i = 0; i<(N+2); i++){
+            for (int j = 0; j<(N+2); j++){
+                for (int k = 0; k<(N+2); k++){
                     Uold[i][j][k] = U[i][j][k];
                 }
             }
-        } 
+        }  
     }      
 }
