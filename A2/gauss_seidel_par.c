6fec14dfedb3333abfd419787c76e0e38c67b6aa
Bastian G <bastianeg1337@gmail.com>
2020-01-16 10:21:33 +0100

    maybe somehting parallel in gaus seidel

--- A2/gauss_seidel_par.c
+++ A2/gauss_seidel_par.c
@@ -9,33 +9,30 @@
 // #include <stdio.h>
 
 void
-gauss_seidel_par(double ***U, double ***F, int N, int iter_max,double tol) {
+gauss_seidel_par(double ***U, double ***F, int N, int iter_max) {
     double deltasq = 4.0/((double) N * (double) N);
     double onesixth = 1.0/6.0;
-    double d = tol+10.0; //inf
     int iter = 0;
     int tempval;
     double squarenorm;
-    while((d>tol) && (iter < iter_max))
+    while(iter < iter_max)
     {
-        d=0;
-        // from  i to j to k
-        // for i
-        for (int i = 1; i<(N+1); i++){
-            //for j
-            for (int j = 1; j<(N+1); j++){
-                //for k
-                for (int k = 1; k<(N+1); k++){
-
-                    // U = 1/6 * (sum of us)
-                    tempval = U[i][j][k];
-                    U[i][j][k] = onesixth*(U[i-1][j][k]+U[i+1][j][k]+U[i][j-1][k]+U[i][j+1][k]+U[i][j][k-1]+U[i][j][k+1]+deltasq*F[i][j][k]);
-                    d += (U[i][j][k]-tempval)*(U[i][j][k]-tempval);
+        #pragma omp parallel default(none) shared(N,onesixth,U,F,deltasq)
+        {
+            
+            #pragma omp for ordered(1)
+            for (int i = 1; i<(N+1); i++){
+                for (int j = 1; j<(N+1); j++){
+                    for (int k = 1; k<(N+1); k++){
+                        
+                        #pragma omp ordered depend(sink:i-1,j-1,k-1)
+                        U[i][j][k] = onesixth*(U[i-1][j][k]+U[i+1][j][k]+U[i][j-1][k]+U[i][j+1][k]+U[i][j][k-1]+U[i][j][k+1]+deltasq*F[i][j][k]);
+                        #pragma omp ordered depend(source)
+                    }
                 }
             }
-        }
+        }//ending parralel region
     // norm calc
-    d = sqrt(d);
 
     // update iteration and Uold
     iter ++;
