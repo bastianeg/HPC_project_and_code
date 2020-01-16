482059a2134e2104f71f19a853ccacf5639d129a
Bastian G <bastianeg1337@gmail.com>
2020-01-16 13:25:46 +0100

    changed 2 to nothing

--- A2/jacobi_par.c
+++ A2/jacobi_par.c
@@ -12,14 +12,15 @@ void
 jacobi_par(double ***U, double ***F, double ***Uold, int N, int iter_max, double tol) {
     double ts, te; // for timing
     double deltasq = 4.0/((double) N * (double) N);
-    //define norm and max_iter and Uold and iter and threshold
     double U1, U2, U3, U4, U5, U6;
     double onesixth = 1.0/6.0;
-    double d = tol+10; //inf
+    double d = tol+10; //initialize norm to inf
+    int iter = 0;
 
     ts = omp_get_wtime(); // start wallclock timer
 
     // update Uold = U
+    #pragma omp parallel for
     for (int i = 0; i<(N+2); i++){
         for (int j = 0; j<(N+2); j++){
             for (int k = 0; k<(N+2); k++){
@@ -29,48 +30,36 @@ jacobi_par(double ***U, double ***F, double ***Uold, int N, int iter_max, double
     }
 
 
-    #pragma omp parallel default(none) shared(N, U1, U2, U3, U4, U5, U6, Uold, onesixth, deltasq, d, tol, iter_max, F, U)
-    {
      //while condition is not satisfied
-     for (int iter = 0; iter < iter_max; iter++){
-         if (d < tol) break;
+    while((d>tol) && (iter < iter_max)){
     
         d = 0.0;
 
         // from  i to j to k
-        #pragma omp for 
+        #pragma omp parallel for reduction(+ : d) default(none) shared(N, U1, U2, U3, U4, U5, U6, Uold, onesixth, deltasq, F, U)
         {
-        for (int i = 1; i<(N+1); i++){
-            //for j
-            for (int j = 1; j<(N+1); j++){
-                //for k
-                for (int k = 1; k<(N+1); k++){
+            for (int i = 1; i<(N+1); i++){
+                //for j
+                for (int j = 1; j<(N+1); j++){
+                    //for k
+                    for (int k = 1; k<(N+1); k++){
 
-                    //update all Us
-                    U1 = Uold[i-1][j][k];
-                    U2 = Uold[i+1][j][k];
-                    U3 = Uold[i][j-1][k];
-                    U4 = Uold[i][j+1][k];
-                    U5 = Uold[i][j][k-1];
-                    U6 = Uold[i][j][k+1];
+                        // U = 1/6 * (sum of us +Delta^2 f)
+                        // this should be unrolled factor 4 in k
+                        U[i][j][k] = onesixth*(Uold[i-1][j][k]+Uold[i+1][j][k]+Uold[i][j-1][k]+Uold[i][j+1][k]+Uold[i][j][k-1]+Uold[i][j][k+1]+deltasq*F[i][j][k]);
 
-                    // U = 1/6 * (sum of us +Delta^2 f)
-                    U[i][j][k] = onesixth*(U1+U2+U3+U4+U5+U6+deltasq*F[i][j][k]);
-
-                    d += (U[i][j][k]-Uold[i][j][k])*(U[i][j][k]-Uold[i][j][k]);
-                }
-              } 
+                        d += (U[i][j][k]-Uold[i][j][k])*(U[i][j][k]-Uold[i][j][k]);
+                    }
+                } 
             }
-        } // end omp for
-
-    } 
-
-        #pragma omp barrier
+        } // end parallel
 
         // norm calc
         d = sqrt(d);
+        iter++;
 
         // update Uold
+        #pragma omp parallel for
         for (int i = 0; i<(N+2); i++){
             for (int j = 0; j<(N+2); j++){
                 for (int k = 0; k<(N+2); k++){
@@ -78,7 +67,7 @@ jacobi_par(double ***U, double ***F, double ***Uold, int N, int iter_max, double
                 }
             }
         }
-    } // end parallel
+    }
     te = omp_get_wtime() - ts;
     printf("Elapsed time: %.5lf\n", te);
 }
