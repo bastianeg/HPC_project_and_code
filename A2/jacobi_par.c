5cd2918c543274b8b35e488230250e2e1d681dbc
bastianeg <bastianeg1337@gmail.com>
2020-01-17 14:43:38 +0100

    fix tol

--- A2/jacobi_par.c
+++ A2/jacobi_par.c
@@ -13,7 +13,7 @@ jacobi_par(double ***U, double ***F, double ***Uold, int N, int iter_max, double
     double ts, te; // for timing
     double deltasq = 4.0/((double) N * (double) N);
     double onesixth = 1.0/6.0;
-    double d = tol+10; //initialize norm to inf
+    double d = tol*N*N*N+10; //initialize norm to inf
     int iter = 0;
 
     ts = omp_get_wtime(); // start wallclock timer
@@ -70,4 +70,5 @@ jacobi_par(double ***U, double ***F, double ***Uold, int N, int iter_max, double
     }
     te = omp_get_wtime() - ts;
     printf("%.5lf, %.5lf\n", te, 1e-9*11*N*N*N*iter/te);
+    printf("Num iter is: %d for tol %.10lf\n",iter,tol);
 }
