b4f920a708350b06be93c4f5ad32f0bb742dc22f
Bastian G <bastianeg1337@gmail.com>
2020-01-22 09:54:31 +0100

    assign1 functions

---
+++ A3/assign1_functions.cu
@@ -0,0 +1,8 @@
+#include <cblas.h>
+
+extern "C" {
+    void matmult_lib(int m, int n, int k, double **A, double **B, double **C){
+
+        cblas_dgemm(CblasRowMajor,CblasNoTrans, CblasNoTrans,m,n,k,1.0,*A,k,*B,n,0.0,*C,n);
+    }
+}
