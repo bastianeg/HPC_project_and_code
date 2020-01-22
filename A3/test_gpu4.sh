1239fcf419e9a99a78b7712bbcf5540eebdda7f3
Bastian G <bastianeg1337@gmail.com>
2020-01-22 14:08:32 +0100

    added job

---
+++ A3/test_gpu4.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#BSUB -q hpcintrogpu
+#BSUB -n 1
+#BSUB -W 15
+#BSUB -J gpu4test
+#BSUB -o gpu4test_%J.out
+#BSUB -N
+#BSUB -R "rusage[mem=4GB]"
+#BSUB -R "span[hosts=1]"
+
+# load the needed compiler here
+module load cuda/10.2
+module load gcc/8.3.0
+
+ELEMPT="2,4,8,16,32"
+CMD=matmult_f.nvcc
+N=800
+TYPE=800
+export MFLOPS_MAX_IT=1
+export MATMULT_COMPARE=0
+
+for ele in $ELEMPT
+do
+    NUM_ELEM_PER_THREAD=$ele ./$CMD $TPYE $N $N $N
+done
