31349392af4ec3e7e60d819cefdce7cdbd0110bc
Bastian G <bastianeg1337@gmail.com>
2020-01-22 11:06:40 +0100

    added stuff

--- A3/matmult_gpun.cu
+++ A3/matmult_gpun.cu
@@ -3,6 +3,22 @@
 extern "C"{
 
     void matmult_gpu1(int m, int n, int k, double *A, double *B, double *C){
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<k; j++){
+                printf("%.2lf ",A[i*n+j]);
+            }
+            printf("\n");
+        }
+        printf("times\n")
+        for(int i = 0; i<k; i++){
+            for(int j = 0; j<n; j++){
+                printf("%.2lf ",B[i*n+j]);
+            }
+            printf("\n");
+        }
+        printf("is\n")
+        
+        
         //allocate memory on GPU
         double* d_A;
         double* d_B;
