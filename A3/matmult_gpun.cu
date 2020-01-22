b1060dbfae83927ed33d6922e59f4e5844fe0617
Bastian G <bastianeg1337@gmail.com>
2020-01-22 10:04:44 +0100

    added files for functions

---
+++ A3/matmult_gpun.cu
@@ -0,0 +1,17 @@
+
+extern "C"{
+
+    void matmult_gpu1(int m, int n, int k, double *A, double *B, double *C){
+
+    }
+    void matmult_gpu2(int m, int n, int k, double *A, double *B, double *C){
+        
+    }
+    void matmult_gpu3(int m, int n, int k, double *A, double *B, double *C){
+        
+    }
+    void matmult_gpu4(int m, int n, int k, double *A, double *B, double *C){
+        
+    }
+
+}
