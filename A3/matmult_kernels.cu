b1d5d0f7269cf4dc679d2011ece04691f28d2fa0
Bastian G <bastianeg1337@gmail.com>
2020-01-22 10:36:23 +0100

    fixed 1d array

--- A3/matmult_kernels.cu
+++ A3/matmult_kernels.cu
@@ -4,14 +4,14 @@ matmult_kernel1(int m, int n, int k, double *A, double *B, double *C){
     // set C to zeros
     for (int i=0;i<m;i++){
         for (int p=0;p<n;p++){
-            C[i][p]=0;
+            C[i*n+p]=0;
         }
     }
     // do matmult with mkn loop order
     for (int i=0;i<m;i++) {
         for (int j=0;j<k;j++){
             for (int p=0;p<n;p++) { 
-                C[i][p]+=A[i][j]*B[j][p];
+                C[i*n+p]+=A[i*k+j]*B[j*n+p];
             }
         }
     }
