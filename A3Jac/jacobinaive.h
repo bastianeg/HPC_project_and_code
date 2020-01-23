419f54a8771862a9cc78104f9ea7451338f1c19c
Schjeldrup <martin@riddler.dk>
2020-01-23 16:06:18 +0100

    daj

--- A3Jac/jacobinaive.h
+++ A3Jac/jacobinaive.h
@@ -3,8 +3,8 @@
  * $Id: jacobi.h,v 1.1 2006/09/28 10:12:58 bd Exp bd $
  */
 
-#ifndef __JACOBINAIVE
-#define __JACOBINAIVE
+#ifndef _JACOBINAIVE
+#define _JACOBINAIVE
 
 void jacobinaive(double *U, double *F, double *Uold, int N, int iter_max);
 
