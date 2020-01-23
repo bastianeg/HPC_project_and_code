f214c8dbe660666eeb44a2be4afb233ce1332a1a
Schjeldrup <martin@riddler.dk>
2020-01-23 13:28:37 +0100

    HEADER

--- A3Jac/jacobinaive.h
+++ A3Jac/jacobinaive.h
@@ -3,8 +3,8 @@
  * $Id: jacobi.h,v 1.1 2006/09/28 10:12:58 bd Exp bd $
  */
 
-#ifndef _JACOBI_H
-#define _JACOBI_H
+#ifndef _JACOBINAIVE
+#define _JACOBINAIVE
 
 void jacobinaive(double *U, double *F, double *Uold, int N, int iter_max, double tol);
 
