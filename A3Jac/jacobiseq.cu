de5f47234c426ce2e00c9114dbc1b3d52608ef3e
Schjeldrup <martin@riddler.dk>
2020-01-23 16:19:40 +0100

    adakn

--- A3Jac/jacobiseq.cu
+++ A3Jac/jacobiseq.cu
@@ -12,9 +12,9 @@ __global__ void
 initmat(int N, double* U, double* Uold, double* F,double deltasq){
     int jmp=N+2;
     int idx;
-    for(int i = 1; i<(N+1); i++){
-        for(int j = 1; j<(N+1); j++){
-            for(int k = 1; k<(N+1); k++){
+    for(int i = 0; i<(N+2); i++){
+        for(int j = 0; j<(N+2); j++){
+            for(int k = 0; k<(N+2); k++){
                 idx = jmp*jmp*i+jmp*j+k;
                 Uold[idx] = U[idx];
                 F[idx] *= deltasq;
