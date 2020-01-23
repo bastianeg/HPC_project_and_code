5275bd429c8e8c1e50a89f0565a14291adc59f4d
Schjeldrup <martin@riddler.dk>
2020-01-23 14:16:53 +0100

    Futch

--- A3Jac/jacobitol.h
+++ A3Jac/jacobitol.h
@@ -8,11 +8,4 @@
 
 void jacobitol(double *U, double *F, double *Uold, int N, int iter_max, double tol);
 
-__global__ void updmat(int N, double* U, double* Uold, int i, int j, int k);
-
-__global__ void jacgpu(int N, double* A, double* b, double* onesixth, int i, int j, int k);
-
-__global__ void initmat(int N, double* U, double* Uold, double* F,double deltasq, int i, int j, int k);
-
-
 #endif
