c9be42c1c08043bed459dacf893c550134fd1b80
Schjeldrup <martin@riddler.dk>
2020-01-24 20:53:37 +0100

    dasdsa

--- A3Jac/jacobimulti.cu
+++ A3Jac/jacobimulti.cu
@@ -35,7 +35,7 @@ jaclower(int jmp, double* U, double* Uold, double* upper_Uold, double* F, double
     int k = blockIdx.z*blockDim.z+threadIdx.z+1; // goes from 1 to N/2  0=1 
 
     int idx=i + j*jmp + k*jmp*jmp;
-    if((i<(jmp-1)) && (j<(jmp-1)) && (k<(jmp-1))){
+    if((i<(jmp-1)) && (j<(jmp-1)) && (k<((jmp-2)/2))){
     if( k == ((jmp-2)/2) ){
         U[idx] = onesixth*(Uold[idx-1]+Uold[idx+1]+Uold[idx-jmp]+\
         Uold[idx+jmp]+Uold[idx-jmp*jmp]+upper_Uold[i+j*jmp]+F[idx]);
@@ -52,7 +52,7 @@ jacupper(int jmp, double* U, double* Uold,double* lower_Uold, double* F, double 
     int i = blockIdx.x*blockDim.x+threadIdx.x+1; // goes from 1 to N
     int j = blockIdx.y*blockDim.y+threadIdx.y+1; // goes from 1 to N
     int k = blockIdx.z*blockDim.z+threadIdx.z;   // goes from 0 to N/2-1   9=4
-    if((i<(jmp-1)) && (j<(jmp-1)) && (k<(jmp-3))){
+    if((i<(jmp-1)) && (j<(jmp-1)) && (k<(((jmp-2)/2)-1))){
     int idx=i + j*jmp + k*jmp*jmp;
 
     if(k==0){
