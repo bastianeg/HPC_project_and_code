f0c4daa6c01f823a3fdc48f491a18518723413dc
Schjeldrup <martin@riddler.dk>
2020-01-24 16:01:57 +0100

    ad

--- A3Jac/jacobitol.cu
+++ A3Jac/jacobitol.cu
@@ -108,7 +108,7 @@ double blockReduceSum(double value) {
      //while condition is not satisfied
      while((d>tol) && (iter < iter_max))
      {
-         printf('A');
+         printf("A");
          jacgpu<<<dim3(N/B,N/B,N/B),dim3(B,B,B)>>>(jmp, U, Uold,F, onesixth);
          cudaDeviceSynchronize();
          
